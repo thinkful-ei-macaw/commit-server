e109ac41618be493749d50f5c76cc041aa353bb4
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-04-21 11:39:52 -0700

    database_url  change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV,
-  DB_URL: process.env.DB_URL || 'postgresql://jordanknox@localhost/commit',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jordanknox@localhost/commit',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
